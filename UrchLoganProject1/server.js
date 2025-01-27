3dea0b408854b6e5bc64d5bba7be5b917851dfaa
LoganUrch <logan.urch@amwins.com>
2025-01-27 11:25:08 -0500

    initial file creation
    create files

---
+++ UrchLoganProject1/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 4893;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
